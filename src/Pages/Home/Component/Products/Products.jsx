4db915bfbd0830aa82a76fe00ca5cdbb8be579fb
ASLAM-stack <arifhossainaslam6@gmail.com>
2024-08-18 10:14:53 +0600

    find products data

--- src/Pages/Home/Component/Products/Products.jsx
+++ src/Pages/Home/Component/Products/Products.jsx
@@ -1,7 +1,17 @@
+import { useQuery } from "@tanstack/react-query";
 import Heading from "../../../../Hooks/Heading";
+import useAxiosPublic from "../../../../Hooks/UseAxiosPublic";
 
 
 const Products = () => {
+    const axiosPublic = useAxiosPublic ()
+    const {data : products = [] } = useQuery({
+        queryKey:['products'],
+        queryFn: async () => {
+            const res = await axiosPublic.get('/products');
+            return res.data
+        }
+    })
     return (
         <div className="max-w-7xl mx-auto">
             <Heading subtitle={'Explore Now'} heading={'Our Products'}></Heading>
